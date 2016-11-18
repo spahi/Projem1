b6282a84b34fea1c0f253fc3880ac51c1342759c
spahi <spahiyahya@outlook.com>
2016-11-18 11:36:05 +0100

    listedeki dosyalar commit olacak

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba dunya  merhaba kosova! devami");
+            Console.ReadLine();
+        }
+    }
+}
