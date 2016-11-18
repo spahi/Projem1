ba2125a8cd78ac11556777d430c118f8d2a90ed1
spahi <spahiyahya@outlook.com>
2016-11-18 13:18:13 +0100

    sonuc

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Merhaba dunya  merhaba kosova! devami");
+            Console.WriteLine("Ikinci sira");
             Console.ReadLine();
         }
     }
